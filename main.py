7577860c0fa164cc81674e33b4a36fc07525b5c9
Dinh-Anh Vu <anhvd27@gmail.com>
2024-01-12 02:27:28 +0700

    ✨ PDF report

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+import io
+
+from fastapi import BackgroundTasks, FastAPI, Request, Response
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
+from xhtml2pdf import pisa
 
 from laptop_analysis.external_resources.web_scraper import web_scraper_resource
 from laptop_analysis.models import Base, Laptop
@@ -15,6 +20,7 @@ Base.metadata.create_all(engine)  # Create the table if it doesn't exist
 session = Session(engine)
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 class LaptopResponse(BaseModel):
@@ -30,6 +36,19 @@ class LaptopAnalysisResponse(BaseModel):
     most_expensive_laptop: LaptopResponse
     most_reviews_laptop: LaptopResponse
     average_storage_capacity: float
+
+
+async def get_analysis():
+    most_expensive_laptop = session.query(Laptop).order_by(Laptop.price.desc()).first()
+    most_reviews_laptop = (
+        session.query(Laptop).order_by(Laptop.review_count.desc()).first()
+    )
+    average_storage_capacity = session.query(func.avg(Laptop.storage_in_gb)).all()[0][0]
+    return {
+        "most_expensive_laptop": most_expensive_laptop,
+        "most_reviews_laptop": most_reviews_laptop,
+        "average_storage_capacity": average_storage_capacity,
+    }
 
 
 @app.get("/")
@@ -61,18 +80,19 @@ async def get_laptops():
 
 @app.get("/laptops/analysis", response_model=LaptopAnalysisResponse)
 async def get_laptops_analysis():
-    most_expensive_laptop = session.query(Laptop).order_by(Laptop.price.desc()).first()
-    most_reviews_laptop = (
-        session.query(Laptop).order_by(Laptop.review_count.desc()).first()
+    return await get_analysis()
+
+
+@app.get("/laptops/analysis/pdf", response_class=FileResponse)
+async def get_laptops_analysis_pdf(request: Request, background_tasks: BackgroundTasks):
+    analysis = await get_analysis()
+    template = templates.TemplateResponse(
+        request=request, name="analysis.html", context=analysis
     )
-    average_storage_capacity = session.query(func.avg(Laptop.storage_in_gb)).all()[0][0]
-    return {
-        "most_expensive_laptop": most_expensive_laptop,
-        "most_reviews_laptop": most_reviews_laptop,
-        "average_storage_capacity": average_storage_capacity,
-    }
-
-
-@app.get("/laptops/analysis/pdf")
-async def get_laptops_analysis_pdf():
-    pass
+    buffer = io.BytesIO()
+    pisa.CreatePDF(src=template.body, dest=buffer)
+    background_tasks.add_task(buffer.close)
+    headers = {"Content-Disposition": 'inline; filename="analysis.pdf"'}
+    return Response(
+        content=buffer.getvalue(), headers=headers, media_type="application/pdf"
+    )
